80cb102f2a73a9b06b1684727eeaae036a67c92f
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-08-10 06:27:19 -0500

    Create Velocity Scene

---
+++ Assets/02_Velocity/Scripts/MyCirCle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCirCle : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void Move() {
+        Debug.Log("MOVING :o");
+    }
+}
