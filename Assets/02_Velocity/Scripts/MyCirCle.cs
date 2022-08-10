9c0c54285ac17ab19e7c4030012b0a1a09f2c47d
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-08-10 07:34:42 -0500

    parte 1

--- Assets/02_Velocity/Scripts/MyCirCle.cs
+++ Assets/02_Velocity/Scripts/MyCirCle.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class MyCirCle : MonoBehaviour
 {
+    [SerializeField] MyVector2D displacement;
+    [SerializeField] Camera mycamera;
+    MyVector2D position;
 
     void Start()
     {
-        
+        position = new MyVector2D(transform.position.x, transform.position.y);
     }
 
 
     void Update()
     {
-        
+        position.Draw(Color.black);
+        displacement.Draw(position, Color.red);
     }
 
     public void Move() {
-        Debug.Log("MOVING :o");
+        position = position + displacement;
+        
+        if(position.x > mycamera.orthographicSize) {
+            displacement.x *= -1;
+            position.x = mycamera.orthographicSize;
+        }
+
+        else if(position.x < mycamera.orthographicSize) {
+
+        }
+
+        transform.position = new Vector3(position.x, position.y);
     }
 }
