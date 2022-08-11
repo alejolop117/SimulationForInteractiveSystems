a4b78c929c140363722a751a877727be185ef020
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-08-10 21:22:54 -0500

    Corrección código

--- Assets/02_Velocity/Scripts/MyCirCle.cs
+++ Assets/02_Velocity/Scripts/MyCirCle.cs
@@ -20,16 +20,27 @@ public class MyCirCle : MonoBehaviour
         displacement.Draw(position, Color.red);
     }
 
-    public void Move() {
+    public void Move() { // Evita que el objeto se salga de los límites de la cámara.
         position = position + displacement;
         
         if(position.x > mycamera.orthographicSize) {
-            displacement.x *= -1;
-            position.x = mycamera.orthographicSize;
+            displacement.x *= -1; //Para cambiar la dirección
+            position.x = mycamera.orthographicSize; //re ubica al objeto en el límite de la cámara.
         }
 
-        else if(position.x < mycamera.orthographicSize) {
+        else if(position.x < -mycamera.orthographicSize) {
+            displacement.x *= -1;
+            position.x = -mycamera.orthographicSize;
+        }
 
+        else if (position.y > mycamera.orthographicSize) {
+            displacement.y *= -1;
+            position.y = mycamera.orthographicSize;
+        }
+
+        else if (position.y < -mycamera.orthographicSize) {
+            displacement.y *= -1;
+            position.y = -mycamera.orthographicSize;
         }
 
         transform.position = new Vector3(position.x, position.y);
