67980910416ee51b9d83dfcb74807681fcd27741
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-08-12 07:37:17 -0500

    displacement and velocity

--- Assets/02_Velocity/Scripts/MyCirCle.cs
+++ Assets/02_Velocity/Scripts/MyCirCle.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class MyCirCle : MonoBehaviour
 {
-    [SerializeField] MyVector2D displacement;
     [SerializeField] Camera mycamera;
+    [SerializeField] MyVector2D velocity;
     MyVector2D position;
+    MyVector2D displacement;
 
     void Start()
     {
@@ -17,29 +18,30 @@ public class MyCirCle : MonoBehaviour
     void Update()
     {
         position.Draw(Color.black);
-        displacement.Draw(position, Color.red);
+        velocity.Draw(position, Color.red);
+        Move();
     }
 
-    public void Move() { // Evita que el objeto se salga de los límites de la cámara.
-        position = position + displacement;
-        
-        if(position.x > mycamera.orthographicSize) {
-            displacement.x *= -1; //Para cambiar la dirección
+    public void Move() { 
+        position = position + velocity * Time.deltaTime; // Time.deltaTime(1 s/ x FPS)
+        // Evita que el objeto se salga de los límites de la cámara.
+        if (position.x > mycamera.orthographicSize) { 
+            velocity.x *= -1; //Para cambiar la dirección
             position.x = mycamera.orthographicSize; //re ubica al objeto en el límite de la cámara.
         }
 
         else if(position.x < -mycamera.orthographicSize) {
-            displacement.x *= -1;
+            velocity.x *= -1;
             position.x = -mycamera.orthographicSize;
         }
 
         else if (position.y > mycamera.orthographicSize) {
-            displacement.y *= -1;
+            velocity.y *= -1;
             position.y = mycamera.orthographicSize;
         }
 
         else if (position.y < -mycamera.orthographicSize) {
-            displacement.y *= -1;
+            velocity.y *= -1;
             position.y = -mycamera.orthographicSize;
         }
 
