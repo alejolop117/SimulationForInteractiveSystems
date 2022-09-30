d7a542d256ba126d8ed9aa68526c484a90237af8
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-09-30 07:17:17 -0500

    Oscillations_2
    Clase 30/09

--- Assets/06_Oscilations/Scripts/Graphs.cs
+++ Assets/06_Oscilations/Scripts/Graphs.cs
@@ -7,6 +7,7 @@ public class Graphs : MonoBehaviour
     [SerializeField] GameObject m_prefab;
     [SerializeField] int m_totalPoints = 10;
     [SerializeField] float distanceFactor = 1;
+    [SerializeField] float amplitude = 1;
     GameObject[] allPoints;
 
     void Start()
@@ -24,8 +25,9 @@ public class Graphs : MonoBehaviour
         for (int i = 0; i < allPoints.Length; i++) {
             float x = i * distanceFactor; //Crear las bolitas en posición distinta
             //Se suma el Time.time para generar el movimiento de la gráfica
-            float y = Mathf.Sin(x + Time.time); //En Y se define el comportamiento de la función
-            allPoints[i].transform.position = new Vector3(x, y, 0);
+            float y = Mathf.Sin(x + Time.time) * amplitude; //En Y se define el comportamiento de la función
+            allPoints[i].transform.localPosition = new Vector3(x, y, 0);
+            //El localPos se referencia con respecto al padre.
         }
     }
 }
