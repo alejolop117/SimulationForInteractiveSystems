7882849107e85ece4fbb8f56c83f41456b1cf8e4
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-10-06 23:18:41 -0500

    Tweens and Easing

--- Assets/07_Tweens/Scripts/Tweens.cs
+++ Assets/07_Tweens/Scripts/Tweens.cs
@@ -2,24 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tweens : MonoBehaviour
-{
+public class Tweens : MonoBehaviour {
     [SerializeField] Transform targetTransform;
     [SerializeField, Range(0, 1)] float normalizedTime;
     [SerializeField] float duration = 1;
+    [SerializeField] Color initialColor;
+    [SerializeField] Color finalColor;
+    [SerializeField] AnimationCurve curve;
 
     Vector3 inicialPos;
+    Vector3 finalPos;
+    SpriteRenderer spriteR;
     float currentTime = 0;
 
     void Start()
     {
-        inicialPos = transform.position;   
+        spriteR = GetComponent<SpriteRenderer>();
+        StartTween();
     }
 
     
     void Update()
     {
-        transform.position = Vector3.Lerp(inicialPos, targetTransform.position, normalizedTime);
+        normalizedTime = currentTime / duration;
+        //transform.position = Vector3.Lerp(inicialPos, finalPos, EaseInQuad(normalizedTime));
+        transform.position = Vector3.Lerp(inicialPos, finalPos, curve.Evaluate(normalizedTime));
+        spriteR.color = Color.Lerp(initialColor, finalColor, EaseInCubic(normalizedTime));
         currentTime += Time.deltaTime;
+
+        if (normalizedTime >= 1) {
+            Debug.Log("Se acabó :c");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) StartTween();
+    }
+
+    void StartTween() {
+        currentTime = 0;
+        inicialPos = transform.position;
+        finalPos = targetTransform.position;
+    }
+
+    float EaseInCubic(float x) {
+        return x * x * x;
     }
 }
