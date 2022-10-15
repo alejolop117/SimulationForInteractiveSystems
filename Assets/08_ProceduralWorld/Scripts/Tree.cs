3e8b5e58742e6a737378ca2b92b2de09bc0366e8
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-10-15 15:33:56 -0500

    ProcedualWorld_Tree

--- Assets/08_ProceduralWorld/Scripts/Tree.cs
+++ Assets/08_ProceduralWorld/Scripts/Tree.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
+    const int INDEX_OF_SQUARE_CHILD = 0;
+    const int INDEX_OF_CIRCLE_CHILD = 1;
     [SerializeField] GameObject branchPrefab;
     [SerializeField] int totalLevels = 3;
+    [SerializeField] float initialSize = 5;
+    [SerializeField, Range(0f, 1f)] float reductionPerLevel = 0.1f;
     Queue<GameObject> rootBranchesQueue = new Queue<GameObject>();
     int currentLevel = 1;
  
     void Start()
     {
         GameObject rootBranch = Instantiate(branchPrefab, transform);
+        ChangeBranchSize(rootBranch, initialSize); 
         rootBranchesQueue.Enqueue(rootBranch);
         GenerateTree();
     }
@@ -24,12 +29,16 @@ public class Tree : MonoBehaviour
 
         ++currentLevel;
 
-        List<GameObject> branchesCreated = new List<GameObject>();
+        float newSize = Mathf.Max(initialSize - initialSize * reductionPerLevel * (currentLevel - 1), 0.1f);
+        var branchesCreated = new List<GameObject>();
 
         while(rootBranchesQueue.Count > 0) {
             var rootBranch = rootBranchesQueue.Dequeue();
-            var rightBranch = CreateBranch(rootBranch, -45f);
-            var leftBranch = CreateBranch(rootBranch, 45f);
+            var rightBranch = CreateBranch(rootBranch, -Random.Range(10,40));
+            var leftBranch = CreateBranch(rootBranch, Random.Range(10, 40));
+
+            ChangeBranchSize(leftBranch, newSize);
+            ChangeBranchSize(rightBranch, newSize);
 
             branchesCreated.Add(leftBranch);
             branchesCreated.Add(rightBranch);
@@ -40,14 +49,38 @@ public class Tree : MonoBehaviour
         }
 
         GenerateTree();
-        
     }
     
     private GameObject CreateBranch(GameObject prevBranch, float relativeAngle) {
         GameObject newBranch = Instantiate(branchPrefab, transform);
-        newBranch.transform.localPosition = prevBranch.transform.localPosition + prevBranch.transform.up;
+        newBranch.transform.localPosition = prevBranch.transform.localPosition + prevBranch.transform.up *
+            GetBranchLenght(prevBranch);
         newBranch.transform.localRotation = 
             prevBranch.transform.localRotation * Quaternion.Euler(0, 0, relativeAngle);
         return newBranch;
+    }
+
+    void ChangeBranchSize(GameObject branchesInstance, float newSize) {
+        var squere = branchesInstance.transform.GetChild(INDEX_OF_SQUARE_CHILD);
+        var circle = branchesInstance.transform.GetChild(INDEX_OF_CIRCLE_CHILD);
+
+        //Square Conf
+        var newScale = squere.transform.localScale;
+        newScale.y = newSize;
+        squere.transform.localScale = newScale;
+
+        var newPosition = squere.transform.localPosition;
+        newPosition.y = newPosition.y = newSize / 2;
+        squere.transform.localPosition = newPosition;
+
+        //Circle Conf
+        var newCirclePosition = circle.transform.localPosition;
+        newCirclePosition.y = newSize;
+        circle.transform.localPosition = newCirclePosition;
+
+    }
+
+    float GetBranchLenght (GameObject branchInstance) {
+        return branchInstance.transform.GetChild(INDEX_OF_SQUARE_CHILD).localScale.y;
     }
 }
